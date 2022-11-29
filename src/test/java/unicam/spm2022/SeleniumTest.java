9ccdd0aaa279a85b5fe393a17029e4ea160653e0
Alessio Galassi <alessio.galassi@studenti.unicam.it>
2022-11-29 10:31:27 +0100

    Added new test with chrome web driver

---
+++ src/test/java/unicam/spm2022/SeleniumTest.java
@@ -0,0 +1,29 @@
+package unicam.spm2022;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+    WebDriver driver;
+
+    @Before
+    void before(){
+        System.setProperty("webdriver.chrome.driver", "c:/users/aless/git/spm2022-mvn-project/driver/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    void checkThisWebAppTitle() throws InterruptedException {
+        driver.get("http://localhost:8080/spm2022/");
+        Thread.sleep(3000);
+        String at = driver.getTitle();
+        String et = "SPM 2022";
+//System.out.println(at);
+        Thread.sleep(4000);
+        Assert.assertEquals(et,at);
+    }
+}
